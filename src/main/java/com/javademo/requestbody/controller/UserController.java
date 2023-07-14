cdc233b5160ebe54b8d9e68f365dc1683edf50ac
okanboke <okanbokee@gmail.com>
2023-07-14 23:36:49 +0300

    "List"

--- src/main/java/com/javademo/requestbody/controller/UserController.java
+++ src/main/java/com/javademo/requestbody/controller/UserController.java
@@ -6,14 +6,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
     @PostMapping("/users")
     public User saveUser(@RequestBody User user) {
-        System.out.println("User saved");
+        System.out.println("User saved!");
         user.setPassword("");
         return user;
+    }
+    @PostMapping("/users-all")
+    public List<User> saveAllUser(@RequestBody List<User> users) { //liste olarak kaydetme
+        System.out.println("All users saved!");
+        users.forEach(user -> user.setPassword(""));
+        return users;
     }
 }

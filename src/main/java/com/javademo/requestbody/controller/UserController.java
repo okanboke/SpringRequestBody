8061467f5d429ede0efdf1598c47ca8079c803a7
okanboke <okanbokee@gmail.com>
2023-07-14 23:41:12 +0300

    "initial commit"

--- src/main/java/com/javademo/requestbody/controller/UserController.java
+++ src/main/java/com/javademo/requestbody/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.javademo.requestbody.controller;
 
 import com.javademo.requestbody.entity.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,10 +15,16 @@ public class UserController {
         user.setPassword("");
         return user;
     }
+
     @PostMapping("/users-all")
     public List<User> saveAllUser(@RequestBody List<User> users) { //liste olarak kaydetme
         System.out.println("All users saved!");
         users.forEach(user -> user.setPassword(""));
         return users;
+    }
+
+    @GetMapping("/header") //header bilgisi örnek
+    public String getHeader(@RequestHeader("My-Header") String myHeader) {
+        return "Your header is : " + myHeader;
     }
 }
